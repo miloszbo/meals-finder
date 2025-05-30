40829089e5e8e67231b5b63e154fcd9f118eebb1
Kacper Gorlewicz <kacpergorlewicz@gmail.com>
2025-05-30 22:46:58 +0200

    UserMenu
    Added Usermenu on main page

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,12 +1,28 @@
+<style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Roboto:wght@700&display=swap');
+</style>
+
+<script setup>
+  import UserMenu from './UserMenu.vue'
+const categories = [
+  "Europe",
+  "Asian",
+  "North American",
+  "South American",
+  "African",
+  "Oceanian"
+];
+</script>
+
 <template>
   <header class="bg-[#0A1118] text-white text-base">
     <div class="flex items-center justify-between p-4">
       <!-- Logo -->
-        <router-link to="/" class="text-5xl font-bold mb-8 hover:text-primary transition-colors duration-200">
-          MealsFinder
-        </router-link>
+      <router-link to="/" class="text-5xl font-bold mb-8 hover:text-primary transition-colors duration-200">
+        MealsFinder
+      </router-link>
 
-      <!-- Pasek wyszukiwania -->
+      <!-- SearchBar -->
       <div class="hidden md:flex justify-center mx-4" style="width: 1088px; height: 48px;">
         <div class="flex w-full bg-[#1a1f26] rounded-full overflow-hidden h-full">
           <input
@@ -20,26 +36,17 @@
         </div>
       </div>
 
-      <!-- Ikony po prawej -->
+      <!-- Icons on left -->
       <div class="flex items-center gap-4 text-lg">
-       <router-link to="/browser" title="Recipes">
-      <i class="fa-solid fa-clipboard-list cursor-pointer hover:text-gray-300"></i>
-       </router-link>
-
-      
-     <!--
-     <a href="/favorites" title="Favorites">
-     <i class="fa-solid fa-heart cursor-pointer hover:text-gray-300"></i>
-      </a>
-     -->
-      <router-link to="/login" title="Profile">
-        <i class="fa-solid fa-user cursor-pointer hover:text-gray-300"></i>
-      </router-link>
-
+        <router-link to="/browser" title="Recipes">
+          <i class="fa-solid fa-clipboard-list cursor-pointer hover:text-gray-300"></i>
+        </router-link>
+        <!-- New dropdown -->
+        <UserMenu />
       </div>
     </div>
 
-    <!-- Kategorie -->
+    <!-- Categories -->
     <nav class="bg-[#0A1118] border-t border-[#1f2a34] flex flex-wrap justify-center gap-6 px-4 py-2">
       <a
         v-for="cat in categories"
@@ -53,18 +60,3 @@
     </nav>
   </header>
 </template>
-
-<script setup>
-const categories = [
-  "Europe",
-  "Asian",
-  "North American",
-  "South American",
-  "African",
-  "Oceanian"
-];
-</script>
-
-<style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Roboto:wght@700&display=swap');
-</style>
