76d56c1657727230a928d6e4efe1c8a4c3d10724
Milosz Borowiecki <96954563+miloszbo@users.noreply.github.com>
2025-06-16 10:33:02 +0200

    Separate user update setting and user add tags logic

--- backend/internal/services/user.service.go
+++ backend/internal/services/user.service.go
@@ -20,7 +20,8 @@ type UserService interface {
 	LoginUser(ctx context.Context, loginData *models.LoginUserRequest) (string, error)
 	CreateUser(ctx context.Context, req *models.CreateUserRequest) error
 	GetUser(ctx context.Context, username string) (repository.GetUserDataRow, error)
-	UpdateUserSettings(ctx context.Context, req *models.UpdateUserSettingsRequest) error
+	UpdateUserSettings(ctx context.Context, req *models.UpdateUserSettingsRequest, username string) error
+	AddUserTag(ctx context.Context, username string, req *models.UserTag) error
 }
 
 type BaseUserService struct {
@@ -73,7 +74,6 @@ func (s *BaseUserService) CreateUser(ctx context.Context, req *models.CreateUser
 		Age:         req.Age,
 		Sex:         req.Sex,
 	})
-
 	if err != nil {
 		log.Println("create user failed:", err)
 		return ErrInternalFailure
@@ -98,15 +98,10 @@ func (s *BaseUserService) generateJWT(username string) (string, error) {
 	return t.SignedString(key)
 }
 
-func (s *BaseUserService) UpdateUserSettings(ctx context.Context, req *models.UpdateUserSettingsRequest) error {
-	// Validate required input
-	if err := req.Validate(); err != nil {
-		return ErrInternalFailure
-	}
-
+func (s *BaseUserService) UpdateUserSettings(ctx context.Context, req *models.UpdateUserSettingsRequest, username string) error {
 	// Update user settings
 	err := s.Repo.UpdateUserSettings(ctx, repository.UpdateUserSettingsParams{
-		Username:    req.Username,
+		Username:    username,
 		Email:       req.Email,
 		Name:        req.Name,
 		Surname:     req.Surname,
@@ -122,58 +117,12 @@ func (s *BaseUserService) UpdateUserSettings(ctx context.Context, req *models.Up
 		return ErrInternalFailure
 	}
 
-	// if user's tag update list isn't empty update tags
-	if req.TagIDs != nil {
-		// Get existing tags
-		existingTags, err := s.Repo.GetUserTags(ctx, req.Username)
-		if err != nil {
-			log.Println("get user tags failed:", err)
-			return ErrInternalFailure
-		}
-
-		// Convert to set
-		existingTagSet := make(map[int32]struct{}, len(existingTags))
-		for _, tagID := range existingTags {
-			existingTagSet[tagID] = struct{}{}
-		}
-
-		incomingTagSet := make(map[int32]struct{}, len(req.TagIDs))
-		for _, tagID := range req.TagIDs {
-			incomingTagSet[tagID] = struct{}{}
-		}
-
-		// Insert new tags
-		for tagID := range incomingTagSet {
-			if _, exists := existingTagSet[tagID]; !exists {
-				err = s.Repo.InsertUserTag(ctx, repository.InsertUserTagParams{
-					Username: req.Username,
-					TagID:    tagID,
-				})
-				if err != nil {
-					log.Println("insert user tag failed:", err)
-					return ErrInternalFailure
-				}
-			}
-		}
-
-		// Delete removed tags
-		for tagID := range existingTagSet {
-			if _, exists := incomingTagSet[tagID]; !exists {
-				err = s.Repo.DeleteUserTag(ctx, repository.DeleteUserTagParams{
-					Username: req.Username,
-					TagID:    tagID,
-				})
-				if err != nil {
-					log.Println("delete user tag failed:", err)
-					return ErrInternalFailure
-				}
-			}
-		}
-	}
-
 	return nil
 }
 
+func (s *BaseUserService) AddUserTag(ctx context.Context, username string, userTag *models.UserTag) error {
+	return nil
+}
 
 // For testing
 type MockUserService struct{}
