8882bc30e1631957d43e24f2fa5e84b5a3eabb89
Marcin Dulik <dulikmarcin03@gmail.com>
2025-06-21 23:08:57 +0200

    url update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://mealsfinder-backend-3ghs7wg7-euc.a.run.app",
     headers: {
         'Content-Type':'application/json'
     },
